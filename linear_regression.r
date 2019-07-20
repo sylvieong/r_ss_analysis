b7955951de544360a895054104d8a6d7a673da6b
Sylvie Ong <sylvie.ong@gmail.com>
2019-07-20 18:04:25 -0500

    Added print statements to display. Commented out extraneous information.

--- linear_regression.r
+++ linear_regression.r
@@ -1,22 +1,29 @@
 data_dir = "C:/Users/Janice_2/Documents/ss_analysis/"
 data_file = "Sample_Dataset_2014c.csv"
 data_path = paste(data_dir, data_file, sep = '')
+print("Reading data from file:")
 print(data_path)
+writeLines("\n")
 
 df_all <- read.table(data_path, header=TRUE, sep=",", quote="\"") # read csv file into a dataframe
+print("Number of rows and columns of input table:") 
 print(dim(df_all))
+writeLines("\n")
 View(df_all) 	# spreadsheet view
-print(str(df_all))	# variable/column types 
+#print(str(df_all))	# variable/column types 
 
 df_regr <- df_all[,c('Height', 'Weight')] # extract 2 columns for regression analysis into a new dataframe 
 
+print("Basic statistics of variables extracted for analysis:")
 print(summary(df_regr)) 	# basic statistics of extracted variables
+writeLines("\n")
 
+print("Correlation coeff (R-value):")
 print(cor(df_regr$Height, df_regr$Weight, use="complete.obs"))	# correlation coefficient between 2 variables, strength of relationship
+writeLines("\n")
 
 linearMod <- lm(Weight ~ Height, data=df_regr)	# fit a linear regression model
 
+#print(linearMod) 	# coefficients of the fitted model
 
-print(linearMod) 	# coefficients of the fitted model
-
-print(summary(linearMod)) 	# p-value and other goodness of fit measures
+#print(summary(linearMod)) 	# p-value and other goodness of fit measures

abc9ab45566ac52b345790db5b7f3c11a60c7453
Sylvie Ong <sylvie.ong@gmail.com>
2019-07-21 11:02:43 -0500

    basic plot.

--- linear_regression.r
+++ linear_regression.r
@@ -1,29 +1,52 @@
 data_dir = "C:/Users/Janice_2/Documents/ss_analysis/"
 data_file = "Sample_Dataset_2014c.csv"
 data_path = paste(data_dir, data_file, sep = '')
-print("Reading data from file:")
+writeLines("\n")
+print("Reading data from file:", quote=FALSE)
 print(data_path)
 writeLines("\n")
 
 df_all <- read.table(data_path, header=TRUE, sep=",", quote="\"") # read csv file into a dataframe
-print("Number of rows and columns of input table:") 
+print("Number of rows and columns of input table:", quote=FALSE) 
 print(dim(df_all))
 writeLines("\n")
 View(df_all) 	# spreadsheet view
 
 df_regr <- df_all[,c('Height', 'Weight')] # extract 2 columns for regression analysis into a new dataframe 
 
-print("Basic statistics of variables extracted for analysis:")
+print("Basic statistics of variables extracted for analysis:", quote=FALSE)
 print(summary(df_regr)) 	# basic statistics of extracted variables
 writeLines("\n")
 
-print("Correlation coeff (R-value):")
+print("Correlation coeff (R-value):", quote=FALSE)
 print(cor(df_regr$Height, df_regr$Weight, use="complete.obs"))	# correlation coefficient between 2 variables, strength of relationship
 writeLines("\n")
 
 linearMod <- lm(Weight ~ Height, data=df_regr)	# fit a linear regression model
 
+library(ggplot2)
+
+# basic plot with fitted regression line
+print("Plotting fitted regression line...", quote=FALSE)
+writeLines("\n")
+plot(ggplot(df_regr, aes(x=Height, y=Weight)) + geom_point(shape=1) +  geom_smooth(method=lm , color="green", se=FALSE))
+
+dev.new()
+# with a little more of color
+print("Plotting fitted regression line with more color...", quote=FALSE)
+writeLines("\n")
+plot(ggplot(df_regr, aes(x=Height, y=Weight)) + geom_point(
+    color="blue",
+        fill="blue",
+        shape=21,
+        alpha=0.2,
+        size=2,
+        stroke = 2) +  geom_smooth(method=lm , color="green", se=FALSE))
+
+
+
 # TODO: get P values - statistical significance of the model
 # TODO: get R2 value
-# TODO: get pretty plots
 
+# TODO: other visualizations and data explorations get pretty plots
+# TODO: include weight/height split by gender
